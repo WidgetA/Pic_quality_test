9cf19d95f220e2bb09019318b02bf4ea220e5d4a
Widget_An <anchunyu@heywhale.com>
2019-11-15 16:03:34 +0800

    Update read_save.py

--- read_save.py
+++ read_save.py
@@ -3,13 +3,16 @@ import numpy as np
 import os
 
 video = './test_video/test_video.mp4'
-
 cap = cv2.VideoCapture(video)
 cap.set(cv2.CAP_PROP_FRAME_COUNT, 4)
 ret = True
 frame_count = 1
 while ret:
     ret, img = cap.read()
+    if len(img) == 0:
+        continue
+    else:
+        pass
     cv2.imwrite(f'./bmp/test{frame_count}.bmp', img)
     cv2.imwrite(f'./png/test{frame_count}.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
     np.save(f'./npy/test{frame_count}.npy', img)
