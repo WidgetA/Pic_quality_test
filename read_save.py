a700ce9437d5db2d92b5f30db04e93882b8eb3e8
Widget_An <anchunyu@heywhale.com>
2019-11-15 15:49:23 +0800

    Update read_save.py

--- read_save.py
+++ read_save.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 
-video = './test_video.mp4'
+video = './test_video/test_video.mp4'
 
 cap = cv2.VideoCapture(video)
 cap.set(cv2.CAP_PROP_FRAME_COUNT, 4)
@@ -11,8 +11,8 @@ frame_count = 1
 while ret:
     ret, img = cap.read()
     frame_count += 1
-    cv2.imwrite(f'./test{frame_count}.bmp', img)
-    cv2.imwrite(f'./test{frame_count}.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
-    np.save(f'./test{frame_count}.npy', img)
+    cv2.imwrite(f'./test_video/test{frame_count}.bmp', img)
+    cv2.imwrite(f'./test_video/test{frame_count}.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+    np.save(f'./test_video/test{frame_count}.npy', img)
 
 os.system("ffmpeg -i ./test_video/test_video.mp4 -f image2 ./ffmpeg/test%d.png")
